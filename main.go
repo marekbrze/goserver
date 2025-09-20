b28579af576d424ccb62244361907281e9878919
Marek Brzezinski <marek.j.brzezinski@gmail.com>
2025-09-20 14:38:30 +0200

    Another lesson

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
+
+func healthCheck(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(http.StatusOK)
+	responseBody := []byte(http.StatusText(http.StatusOK))
+
+	_, err := writer.Write(responseBody)
+	if err != nil {
+		// Log any error that occurs during writing the response.
+		log.Println("Failed to write response:", err)
+	}
+}
 
 func main() {
 	serverMux := http.NewServeMux()
-	server := http.Server{
+	serverMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	serverMux.HandleFunc("/healthz", healthCheck)
+	server := &http.Server{
 		Handler: serverMux,
 		Addr:    ":8080",
 	}
