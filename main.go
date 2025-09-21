7d97c543408bce5b0017a5b1ac76fbea157df380
Marek Brzezinski <marek.j.brzezinski@gmail.com>
2025-09-21 20:15:18 +0200

    Handling hits works

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverhits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverhits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) getNumberOfHits(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	responseString := fmt.Sprintf("Hits: %d", cfg.fileserverhits.Load())
+	responseBody := []byte(responseString)
+
+	_, err := w.Write(responseBody)
+	if err != nil {
+		// Log any error that occurs during writing the response.
+		log.Println("Failed to write response:", err)
+	}
+}
 
 func healthCheck(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -18,9 +44,13 @@ func healthCheck(writer http.ResponseWriter, request *http.Request) {
 }
 
 func main() {
+	apiCfg := apiConfig{
+		fileserverhits: atomic.Int32{},
+	}
 	serverMux := http.NewServeMux()
-	serverMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	serverMux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	serverMux.HandleFunc("/healthz", healthCheck)
+	serverMux.HandleFunc("/metrics", apiCfg.getNumberOfHits)
 	server := &http.Server{
 		Handler: serverMux,
 		Addr:    ":8080",
